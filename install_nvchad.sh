2c976d440ebcb5b99ec4aa97b8c10d62610e17e2
schroe0690 <schroedinger.gs@gmail.com>
2025-05-06 18:29:20 +0900

    installスクリプトのディレクトリ修正

--- install_nvchad.sh
+++ install_nvchad.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # 環境変数の設定
-export XDG_CONFIG_HOME=/root/.config
-export XDG_DATA_HOME=/root/.local/share
-export XDG_CACHE_HOME=/root/.cache
+export XDG_CONFIG_HOME="$HOME/config"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_CACHE_HOME="$HOME/.cache"
 
 # Neovimの最新版をインストール
 git clone https://github.com/neovim/neovim.git
@@ -16,12 +16,12 @@ rm -rf neovim
 
 ### 以下, nvchadの設定
 # 既存の設定を削除
-rm -rf /root/.config/nvim
-rm -rf /root/.local/share/nvim
-rm -rf /root/.cache/nvim
+rm -rf "$HOME/.config/nvim"
+rm -rf "$HOME/.local/share/nvim"
+rm -rf "$HOME/.cache/nvim"
 
 # NvChadのインストール
-git clone https://github.com/NvChad/starter /root/.config/nvim --depth 1
+git clone https://github.com/NvChad/starter "$HOME/.config/nvim" --depth 1
 
 # プラグインの同期
 nvim --headless +"Lazy sync" +qa
